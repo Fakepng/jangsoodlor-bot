33cf53613c41309a69ff3ad8d9956fb850761228
Fakepng <86796781+Fakepng@users.noreply.github.com>
2021-11-08 22:51:05 +0700

    fix avatar

--- commands/avatar.js
+++ commands/avatar.js
@@ -3,11 +3,11 @@ module.exports = {
     description: "Show an avatar",
     execute(message, args){
         if (!message.mentions.users.size) {
-            return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: 'png', dynamic: true })}>`);
+            return message.channel.send(`Your avatar: ${message.author.displayAvatarURL({ format: 'png', dynamic: true })}`);
         }
         
         const avatarList = message.mentions.users.map(user => {
-            return `${user.username}'s avatar: <${user.displayAvatarURL({ format: 'png', dynamic: true })}>`;
+            return `${user.username}'s avatar: ${user.displayAvatarURL({ format: 'png', dynamic: true })}`;
         });
         
         // Send the entire array of strings as a message
