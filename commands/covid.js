07cced17bd109889d608293b55e1ea349a634082
Fakepng <86796781+Fakepng@users.noreply.github.com>
2021-11-09 13:07:13 +0700

    Update covid.js

--- commands/covid.js
+++ commands/covid.js
@@ -3,7 +3,7 @@ module.exports = {
     description: "Covid!",
     execute(message, args){
             const axios = require('axios')
-            axios.get('	https://covid19.ddc.moph.go.th/api/Cases/today-cases-all').then(function (response) {
+            axios.get('https://covid19.ddc.moph.go.th/api/Cases/today-cases-all').then(function (response) {
                 message.channel.send(`จำนวนผู้ติดเชื้อรายใหม่: ${response.data[0].new_case} \nจำนวนผู้ติดเชื้อสะสม: ${response.data[0].total_case} \nจำนวนผู้เสียชีวิตรายใหม่: ${response.data[0].new_death} \nจำนวนผู้เสียชีวิตสะสม: ${response.data[0].total_death} \nจำนวนผู้รักษาหายรายใหม่: ${response.data[0].new_recovered} \nจำนวนผู้รักษาหายสะสม: ${response.data[0].total_recovered}`);
             }).catch(function (error) {
                 console.log(error);
