78a0dad18abb6faf67ea14696f58543e67024dac
Fakepng <86796781+Fakepng@users.noreply.github.com>
2021-11-13 21:06:39 +0700

    Update waifu.js

--- commands/waifu.js
+++ commands/waifu.js
@@ -5,22 +5,30 @@ module.exports = {
     name: 'waifu',
     description: "waifu",
     execute(message, args){
-        if(args == 'help'){
-            message.channel.send(`Usage: ${config.PREFIX}waifu`);
-        }else if(!args[0]){
+        if (!args[0]) {
             axios.get(`https://api.waifu.pics/sfw/waifu`).then(function (response) {
                 message.channel.send(`${response.data.url}`);
             }).catch(function (error) {
                 console.log(error);
-            })
+            })            
+        }else if(args == 'help'){
+            message.channel.send(`Usage: ${config.PREFIX}waifu <Category>\nNoting or (neko, shinobu, megumin, bully, cuddle, cry, hug, awoo, kiss, lick, pat, smug, bonk, yeet, blush, smile, wave, highfive, handhold, nom, bite, glomp, slap, kill, kick, happy, wink, poke, dance, cringe)`);
         }else if(args[0] == 'nsfw'){
-            axios.get(`https://api.waifu.pics/nsfw/waifu`).then(function (response) {
+            if(!args[1]){
+                message.channel.send(`${config.PREFIX}waifu nsfw <waifu, neko, trap, blowjob>`)
+            }else {
+                axios.get(`https://api.waifu.pics/nsfw/${args[1]}`).then(function (response) {
+                    message.channel.send(`${response.data.url}`);
+                }).catch(function (error) {
+                    console.log(error);
+                })                
+            }
+        }else {
+            axios.get(`https://api.waifu.pics/sfw/${args[0]}`).then(function (response) {
                 message.channel.send(`${response.data.url}`);
             }).catch(function (error) {
                 console.log(error);
             })
-        }else {
-            message.channel.send(`Usage: ${config.PREFIX}waifu ${config.PREFIX}fox nsfw`);
         }
     }
 }
