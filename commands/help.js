245ebc71b8ecdcc59d030670d07371d4a25b0932
Fakepng <86796781+Fakepng@users.noreply.github.com>
2021-11-28 20:14:14 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ const i18n = require("../util/i18n");
 
 module.exports = {
   name: "help",
-  aliases: ["h"],
+  aliases: ["?"],
   description: i18n.__("help.description"),
   execute(message) {
     let helpEmbed = new MessageEmbed()
@@ -12,7 +12,7 @@ module.exports = {
       .setColor("#F8AA2A")
       .addFields(
         { name: 'Music', value: 'loop(l), lyrics(ly), move(mv), np, pause, play(p), playlist(pl), queue(q), remove(rm), resume(r), search, shuffle, skip(s), skipto(st), stop, volume(v)' },
-        { name: 'Orginal', value: 'help(h), invite, ping, uptime' },
+        { name: 'Orginal', value: 'help(?), invite, ping, uptime' },
         { name: 'Information', value: 'avatar, axolotl, bored, cat, covid, dict, dog, duck, fact, food, forza, fox, gif, http, iss, joke, member, meme, number, pic, quote, salim, waifu' },
         { name: 'Response', value: 'abottleofwater(abow), hello, joker, lanna, rules, say' },
         { name: 'Game', value: 'flip, tictactoe(ttt)' },
