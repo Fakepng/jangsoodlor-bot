6c17a68cc44661ee384c34621edb3a40927210fa
Fakepng <86796781+Fakepng@users.noreply.github.com>
2021-12-10 20:23:12 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -11,7 +11,7 @@ module.exports = {
       .setDescription(i18n.__("help.embedDescription"))
       .setColor("#F8AA2A")
       .addFields(
-        { name: 'Music', value: '```loop(l), lyrics(ly), move(mv), np, pause, play(p), playlist(pl), queue(q), remove(rm), resume(r), search, shuffle, skip(s), skipto(st), stop, volume(v)```' },
+        { name: 'Music', value: '```clip, clips, loop(l), lyrics(ly), move(mv), np, pause, play(p), playlist(pl), queue(q), remove(rm), resume(r), search, shuffle, skip(s), skipto(st), stop, volume(v)```' },
         { name: 'Orginal', value: '```help(?), invite, ping, uptime```' },
         { name: 'Information', value: '```avatar, axolotl, bored, cat, covid, dict, dog, duck, fact, food, forza, fox, gif, http, iss, joke, member, meme, number, pic, quote, salim, waifu```' },
         { name: 'Response', value: '```abottleofwater(abow), hello, joker, lanna, rules, say```' },
