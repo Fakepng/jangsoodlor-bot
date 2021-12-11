b0bdda23b619910db42c965be870a5cb52321225
Fakepng <86796781+Fakepng@users.noreply.github.com>
2021-12-12 00:57:20 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Downloading and installing the latest version of the project..."
+rm -rf ~/jangsoodlor-bot
 git clone https://github.com/Fakepng/jangsoodlor-bot/
 echo "Installing dependencies..."
 cd jangsoodlor-bot
@@ -8,7 +9,7 @@ echo "Copying config file..."
 cd
 cp config.json jangsoodlor-bot
 echo "Copying sound file..."
-rm -rf ~/jangsoodlor-bot/sounds
+rm ~/jangsoodlor-bot/sounds/putmusichere.mp3
 cd sounds
 cp * ~/jangsoodlor-bot/sounds
 echo "Starting the bot..."
