352b2a54bdf937b29e6eef7d4a52914edfcb97c6
Fakepng <86796781+Fakepng@users.noreply.github.com>
2021-12-29 13:57:50 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -13,7 +13,7 @@ module.exports = {
       .addFields(
         { name: 'Music', value: '```clip, clips, loop(l), lyrics(ly), move(mv), np, pause, play(p), playlist(pl), queue(q), remove(rm), resume(r), search, shuffle, skip(s), skipto(st), stop, volume(v)```' },
         { name: 'Orginal', value: '```help(?), invite, ping, uptime```' },
-        { name: 'Information', value: '```avatar, axolotl, bored, cat, covid, dict, dog, duck, fact, food, forza, fox, gif, http, iss, joke, member, meme, number, pic, quote, salim, waifu```' },
+        { name: 'Information', value: '```avatar, axolotl, bored, cat, covid, dict, dog, duck, fact, food, forza, fox, gif, http, iss, joke, member, meme, number, pic, quote, random(ran), salim, waifu```' },
         { name: 'Response', value: '```abottleofwater(abow), hello, joker, lanna, rules, say```' },
         { name: 'Game', value: '```flip, tictactoe(ttt)```' },
         { name: 'Economy', value: '```balance(bal), dailyreward(dr), deposit(dep), pay, void, withdraw(wd)```' },
